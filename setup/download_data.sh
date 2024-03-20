2cb717c3d3b9f72acfb26e98abd244339e460fa8
Mark Baushenko <e0xextazy@gmail.com>
2024-03-20 17:38:51 +0300

    Update download_data.sh

--- setup/download_data.sh
+++ setup/download_data.sh
@@ -1,4 +1,4 @@
 pip install gdown==4.5.4
-gdown 1s_zzx_C6p_8WTljwIKzWQUFyZaX1AWnS
-gdown 1zXanF6vCfnYTshhBWodixb9QtoRprPhq
-gdown 13_FZT5PLaOlHO9FrzfvwO5IosqnaJThi
+gdown 1X7ezp7FAjtzpwueuyLXNjuukfI6dkhWp
+gdown 1pFm30rf5TOheLA3lFEeex8fmaSZti2L2
+gdown 1JVKnzERZ9TWjiov2Xv0jGT4bXHdum5Rg
